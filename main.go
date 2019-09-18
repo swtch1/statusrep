4a3767fb984062bf4937a2f1ea30433bed37bddb
Joshua Thornton <joshua_thornton1@homedepot.com>
2019-09-18 01:14:54 -0400

    finalizing main with report writer

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"os"
+	"sync"
+	"time"
 )
 
 // buildVersion should be populated at build time by build ldflags
 var buildVersion string
 
+func init() {
+	Apps = make(map[Application]Metric)
+}
+
 func main() {
+	start := time.Now()
+
 	var flag Flag
 	flag.Version = buildVersion
 	flag.Parse()
@@ -23,14 +32,44 @@ func main() {
 
 	hosts, err := ReadAllHosts(f)
 	if err != nil {
-		log.WithError(err).Fatalf("unable to open file '%s'", flag.HostsFile)
+		log.WithError(err).Fatal("unable to read in hosts")
 	}
 
-	for _, host := range hosts {
-		statusURL, err := HostStatusURL(flag.RootURL, host)
-		if err != nil {
-			log.WithError(err).Errorf("could not get status for host '%s'", host)
+	var wg sync.WaitGroup
+	for _, h := range hosts {
+		wg.Add(1)
+		go func(h string) {
+			defer wg.Done()
+
+			statusURL, err := HostStatusURL(flag.RootURL, h)
+			if err != nil {
+				log.WithError(err).Errorf("could not create URL for host '%s'", h)
+			}
+			host := Host{URL: statusURL}
+
+			status, err := host.RequestHostStatus()
+			if err != nil {
+				log.WithError(err).Errorf("could not get status for host '%s'", h)
+			}
+			IncrementCounters(Apps, status)
+		}(h)
+	}
+	wg.Wait()
+
+	writeReport(os.Stdout)
+	fmt.Printf("\nreport completed in %s\n", time.Now().Sub(start).Truncate(time.Millisecond))
+}
+
+func writeReport(w io.Writer) {
+	for app, metrics := range Apps {
+		var successRate float32
+		if metrics.TotalSuccessCount == 0 || metrics.TotalRequestsCount == 0 {
+			successRate = 0
+		} else {
+			successRate = float32(metrics.TotalSuccessCount) / float32(metrics.TotalRequestsCount)
 		}
-		fmt.Println(statusURL)
+		if _, err := fmt.Fprintf(w, "%s,%s,%.2f\n", app.Name, app.Version, successRate); err != nil {
+			log.WithError(err).Error("invalid printer format")
+		}
 	}
 }
